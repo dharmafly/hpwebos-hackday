5101ec95af79b93b0293ecf11565ea8777118406
Premasagar Rose <p@dharmafly.com>
2011-07-10 10:03:35 -0400

    Loading datasets in series, after images in each set have loaded

--- app/js/main.js
+++ app/js/main.js
@@ -2,12 +2,12 @@
     "use strict";
 
   var width = window.innerWidth,
-      columns = 3,
-      gutter = 10,
       db = createFakeDb(),
       articlesTemplate = jQuery(".articles").html(),
       contentSections = jQuery("#scroller > div.windows > section .articles-container"),
-      compassDirections = {"nw":0, "n":1, "ne":2, "w":3, "x":4, "e":5, "sw":6, "s":7, "se":8};
+      compassDirections = {"nw":0, "n":1, "ne":2, "w":3, "x":4, "e":5, "sw":6, "s":7, "se":8},
+      compassKeys = _(compassDirections).chain().keys().without("x").value(),
+      currentlyProcessing;
   
   function loadDb(callback){
     db.fetchDataset(callback);
@@ -17,19 +17,23 @@
     var img = container.find("img"),
         imgToLoad = img.length,
         loaded = 0;
-        
-    img.bind("load error", function(){
-      loaded ++;
-      if (loaded === imgToLoad){
-        callback(container);
-      }
-    });
+    
+    if (imgToLoad){
+      img.bind("load error", function(){
+        loaded ++;
+        if (loaded === imgToLoad){
+          callback(container);
+        }
+      });
+    }
+    
+    callback(container);
   }
 
   function layoutNewspaper(container){
     container.masonry({
       itemSelector:"article",
-      gutterWidth: gutter,
+      gutterWidth: 20,
       columnWidth: 300
     });
   }
@@ -45,14 +49,34 @@
         container = getElementByCompass(compass).html(html);
     
     layoutNewspaper(container);
-    onImagesLoaded(container, layoutNewspaper);
+    
+    onImagesLoaded(container, function(){
+      layoutNewspaper(container);
+      renderNextNeighbour(renderWindow);
+    });
+  }
+  
+  function renderNextNeighbour(callback){
+    var compass;
+
+    currentlyProcessing = currentlyProcessing ? currentlyProcessing + 1 : 0;
+    compass = compassKeys[currentlyProcessing];
+    
+    if (compass){
+      db.getNeighbourData(compass, function(data){
+        callback(compass, data);
+      });
+    }
+    else {
+      currentlyProcessing = 0;
+    }
   }
 
   function init() {
     // Centre location
     loadDb(function(data) {
       renderWindow("x", data);
-      db.getAllNeighbourData(renderWindow);
+        //db.getAllNeighbourData(renderWindow);
     });
   }
 
